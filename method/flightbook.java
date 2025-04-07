b3b25d09e1e058d5383091b09ed48b2035632ffd
viswesh-web <visweshpenumarthi@gmail.com>
2025-04-08 00:31:55 +0530

    Create flightbook.java

---
+++ method/flightbook.java
@@ -0,0 +1,23 @@
+[BOOK] 
+void bookTicket(String name) { 
+} 
+System.out.println("Ticket booked for: 
++ name 
+void bookTicket(String name, String seatClass) { 
+} 
+System.out.println("Ticket booked for: " + name + 
+" in " + seatclass + " class."); 
+System.out.println("Ticket booked for: 
++ name + 
+" in " + seatclass + " class with " 
++ mealPreference + 
+meal."); 
+void bookTicket(Strin Loading... ring seatClass, String mealPreference) { 
+} 
+} 
+public class Flight BookingDemo { 
+public static void main(String[] args) { 
+Flight Booking booking = new Flight Booking(); 
+booking.bookTicket("Alice"); 
+booking.bookTicket ("Bob", "Business"); 
+booking.bookTicket("Charlie", "Economy", "Vegetarian");
